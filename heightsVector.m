29197de24ac4ada84e369add9776bf17baa5b606
Sadid Rahman <124613563+Sadid-Rahamn-07@users.noreply.github.com>
2023-03-13 09:11:19 +0600

    Update heightsVector.m

--- heightsVector.m
+++ heightsVector.m
@@ -7,12 +7,25 @@ for a = 1 : 1
     height4 = input("Enter a height in m : ");
     height5 = input("Enter a height in m : ");
 end
+
 % storing the user input in to an array called arr1
 arr1 = [height1,height2,height3,height4,height5];
+
+% display the 1st element of the array 
 disp(arr1(1));
+
+% display the 2nd element of the array 
 disp(arr1(2));
+
+% display the 3rd element of the array 
 disp(arr1(3));
+
+% display the 4th element of the array 
 disp(arr1(4));
+
+% display the 5th element of the array 
 disp(arr1x(5));
+
+
 
 
