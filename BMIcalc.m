1859422ad310d5ab76812fc2c3480873909b1ea6
Sadid Rahman <124613563+Sadid-Rahamn-07@users.noreply.github.com>
2023-03-13 08:39:09 +0600

    Update BMIcalc.m

--- BMIcalc.m
+++ BMIcalc.m
@@ -5,8 +5,8 @@ weight = input("Enter your weight in kgs : ");
 height = input("Enter your height in m : ");
 
 % BMU = weight/(height^2)
-BMI = weight / (height* height);
+bmi = weight / (height* height);
 
 % Print the BMI of the user
-fprintf ("Your BMI is : %.2f", BMI) ;
+fprintf ("Your BMI is : %.2f", bmi) ;
 
