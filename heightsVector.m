333f04107ac5fd9d6a583d19245ba55182ef202b
Sadid Rahman <124613563+Sadid-Rahamn-07@users.noreply.github.com>
2023-03-13 08:27:47 +0600

    Add files via upload

---
+++ heightsVector.m
@@ -0,0 +1,16 @@
+
+for a = 1 : 1
+    height1 = input("Enter a height in m : ");
+    height2 = input("Enter a height in m : ");
+    height3 = input("Enter a height in m : ");
+    height4 = input("Enter a height in m : ");
+    height5 = input("Enter a height in m : ");
+end
+x = [height1,height2,height3,height4,height5];
+disp(x(1));
+disp(x(2));
+disp(x(3));
+disp(x(4));
+disp(x(5));
+
+
---
+++ sumAbs.m
@@ -0,0 +1,15 @@
+
+
+total = 0;
+for a = 1: +1 : 5 
+    
+    num_1 = abs(input("\nEnter an integer : "));
+    
+    
+    total = total + num_1;
+
+  
+    
+
+end
+fprintf("Sum of absolute num : %0.1f",total);
