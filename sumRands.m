350cf58ec89ae4f4fe5b9ed1094cbde985a42d7e
Sadid Rahman <124613563+Sadid-Rahamn-07@users.noreply.github.com>
2023-03-13 09:00:55 +0600

    Update sumRands.m

--- sumRands.m
+++ sumRands.m
@@ -1,18 +1,21 @@
 
 total = 0;
+
+% loops 5 times
 for x = 1 : +1 : 5
-    number = randi(6,1) ;  
+    % random number generator
+    number = randi(6,1);
+    % sum of the random numbers  
     total = total + number;
+    % showing the value of the random number for each iteration of the loop 
     fprintf("\nRand num %d : %d",x,number);
-   
-    
-    
-
-    
 end
+% max value of randi 
 fprintf("\nMax : 6");
-fprintf("\nMin :1");
+% min value of randi
+fprintf("\nMin : 1");
+% sum of all the random numbers
 fprintf("\nSum of rand numbers : %d",total);
-
+# average sum of random numbers 
 fprintf("\nSum_Average : %0.2f",total/5);
 
