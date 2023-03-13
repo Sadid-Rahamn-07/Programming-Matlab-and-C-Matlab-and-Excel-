644200258ddd653757142c9eb593704fe2a8041c
Sadid Rahman <124613563+Sadid-Rahamn-07@users.noreply.github.com>
2023-03-13 09:06:37 +0600

    Update sumAbs.m

--- sumAbs.m
+++ sumAbs.m
@@ -1,15 +1,12 @@
 
-
+% declaring the total variable for storing the sum of absolute values from the user 
 total = 0;
 for a = 1: +1 : 5 
-    
-    num_1 = abs(input("\nEnter an integer : "));
-    
-    
+    % using the num_1 variable to store the value given by the user while also converting it to a absolute value
+    % therefore, all pos and neg value will be converted to positive
+    num_1 = abs(input("\nEnter an integer : "));    
+    % adding the values given by the user to find the total sum
     total = total + num_1;
-
-  
-    
-
 end
+% output the sum of absolute values given  
 fprintf("Sum of absolute num : %0.1f",total);
