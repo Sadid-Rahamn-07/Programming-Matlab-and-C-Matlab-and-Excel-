0160418925500fde37242fa40c575f9b0b8d587c
Sadid Rahman <124613563+Sadid-Rahamn-07@users.noreply.github.com>
2023-03-13 09:09:45 +0600

    Update heightsVector.m

--- heightsVector.m
+++ heightsVector.m
@@ -1,16 +1,18 @@
-
+% loop once
 for a = 1 : 1
+    % These variable stores the height given by the user
     height1 = input("Enter a height in m : ");
     height2 = input("Enter a height in m : ");
     height3 = input("Enter a height in m : ");
     height4 = input("Enter a height in m : ");
     height5 = input("Enter a height in m : ");
 end
-x = [height1,height2,height3,height4,height5];
-disp(x(1));
-disp(x(2));
-disp(x(3));
-disp(x(4));
-disp(x(5));
+% storing the user input in to an array called arr1
+arr1 = [height1,height2,height3,height4,height5];
+disp(arr1(1));
+disp(arr1(2));
+disp(arr1(3));
+disp(arr1(4));
+disp(arr1x(5));
 
 
