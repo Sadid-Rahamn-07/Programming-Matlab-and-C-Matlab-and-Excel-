a99fa8fdbca349f99741199a4a31d90c01c87296
Sadid Rahman <124613563+Sadid-Rahamn-07@users.noreply.github.com>
2023-03-20 11:54:16 +0600

    Update q1a.m

--- q1a.m
+++ q1a.m
@@ -1,9 +1,10 @@
 % loops until i equal to the number of elements in A
-A = [-10:0.625:10];
+A = [-10:1:10]
 B = [];
 for i = 1:1:len
-    B(end + i) = A(i) * A(i) * A(i);
+    B(i) = A(i) * A(i) * A(i);
     %outputs the elements of A cubed
     
 end
 fprintf("%.4f ",B(:));
+
