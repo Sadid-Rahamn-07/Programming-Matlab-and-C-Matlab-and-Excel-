407f36a2d465df6448964fe204dc9d943f288625
Sadid Rahman <124613563+Sadid-Rahamn-07@users.noreply.github.com>
2023-03-20 11:44:30 +0600

    Update q1c.m

--- q1c.m
+++ q1c.m
@@ -1,3 +1,10 @@
+main = sort(A);
+
+close all;  %close all figure windows
+hold on; % display plots on the same window for now
+plot(B);
+plot(C);
+hold off;  % turn off plotting on the same window. 
 
 for x = 1:len
     plot(Y);
